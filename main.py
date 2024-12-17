5fc348be64606480138e47e3a102fef3263d9582
asithniwantha <asithniwantha@gmail.com>
2024-12-18 00:09:39 +0530

    add relaySwitcher

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import wifimgr
-import machine # type: ignore
-from machine import Pin, SoftI2C # type: ignore
+import machine  # type: ignore
+from machine import Pin, SoftI2C  # type: ignore
 import asyncio
 import _thread
 import socket
@@ -15,10 +15,14 @@ tank_switch_pin = machine.Pin(36, machine.Pin.IN, Pin.PULL_UP)
 
 global server
 global ip_address
+global relayState
 
+relayState = False
 set_on_time = 0
 set_off_time = 0
 timer_seconds = 0
+elapsed_time_s = 0
+elapsed_time_m = 0
 
 # using default address 0x3C
 i2c = SoftI2C(sda=Pin(13), scl=Pin(14))
@@ -28,35 +32,65 @@ display = ssd1306.SSD1306_I2C(128, 32, i2c)
 async def timer_counter():
     global timer_seconds
     global ip_address
-    global server
-    server = None
+    global myconfig
+    global relayState
+    global elapsed_time_s
+    global elapsed_time_m
     ip_address = "wait for wifi"
 
+    myconfigfile = ConfigFile('configuration.conf')
+    myconfigfile.read()
+    myconfig = myconfigfile.config
+    onTimeStr = str(myconfig['timer']['ontime'])
+    offTimeStr = str(myconfig['timer']['offtime'])
+
     while True:
-        print("timer counter : ", timer_seconds)
+        elapsed_time_s = int(timer_seconds % 60)
+        elapsed_time_m = int(timer_seconds/60)
+        print("timer counter : ", timer_seconds,
+              "       elapsed time  >>", elapsed_time_m, " : ", elapsed_time_s)
         display.fill(0)
         display.text(str(ip_address), 0, 0, 1)
-        if server is not None:
-            display.text(str(server.on_time_value), 0, 13, 1)
-            display.text(str(server.off_time_value), 64, 13, 1)
-        display.text(str(int(timer_seconds/60)), 0, 24, 1)
-        display.text(" : ", 10, 24, 1)
-        display.text(str(int(timer_seconds % 60)), 20, 24, 1)
+        if relayState:
+            display.text(onTimeStr + " <", 0, 13, 1)
+            display.text(offTimeStr, 64, 13, 1)
+        else:
+            display.text(onTimeStr, 0, 13, 1)
+            display.text("> " + offTimeStr, 64, 13, 1)
+        display.text(str(int(timer_seconds/60)), 10, 24, 1)
+        display.text(":", 50, 24, 1)
+        display.text(str(int(timer_seconds % 60)), 60, 24, 1)
         display.show()
         await asyncio.sleep_ms(1000)  # type: ignore
-        timer_seconds += 1 
+        timer_seconds += 1
 
 
-async def p():
+async def relaySwitcher():
+    global timer_seconds
+    global elapsed_time_m
+    global myconfig
+    global relayState
+    relayState = False
+
     while True:
-        a = 0
-        print("p")
-        await asyncio.sleep(4)
+        if relayState:
+            if elapsed_time_m >= myconfig['timer']['ontime']:
+                relayState = False
+                timer_seconds = 0
+                relay_out_pin.off()
+                print("relay turned off")
+        else:
+            if elapsed_time_m >= myconfig['timer']['offtime']:
+                relayState = True
+                timer_seconds = 0
+                relay_out_pin.on()
+                print("relay turned on")
+        await asyncio.sleep(1)
 
 
 async def main():
-    pfn = asyncio.create_task(p())
-    ppfn = asyncio.create_task(timer_counter())
+    timerTask = asyncio.create_task(timer_counter())
+    relaySwitcherTask = asyncio.create_task(relaySwitcher())
     while True:
         await asyncio.sleep(.001)
     machine.reset()
@@ -65,12 +99,19 @@ async def main():
 def configure_main(delay, id):
     global server
     global ip_address
+    global myconfig
+
+    myconfigfile = ConfigFile('configuration.conf')
+    myconfigfile.read()
+    myconfig = myconfigfile.config
+    print("configurations loaded")
+
     print("starting server")
     wlan = wifimgr.get_connection()
     if wlan is None:
         print("Could not initialize the network connection.")
         while True:
-            pass  
+            pass
     # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
     print("ESP OK")
     ip_address = wlan.ifconfig()[0]
@@ -83,28 +124,31 @@ def configure_main(delay, id):
     except OSError as e:
         machine.reset()
 
-    myconfigfile = ConfigFile('configuration.conf')
-    myconfigfile.read()
-    myconfig = myconfigfile.config
-    pfn = asyncio.create_task(p())
-
     server = AJXServer.AJXServer(s)
     server.on_time_value = myconfig['timer']['ontime']
     server.off_time_value = myconfig['timer']['offtime']
     print(server.on_time_value)
     print(server.off_time_value)
 
+    configChanged = False
     while True:
         server.start()
         server._elapsed_time = timer_seconds
         if myconfig['timer']['ontime'] != server.on_time_value:
+            configChanged = True
             myconfig['timer']['ontime'] = server.on_time_value
             print("config change", server.on_time_value)
             myconfigfile.write(myconfig)
         if myconfig['timer']['offtime'] != server.off_time_value:
+            configChanged = True
             myconfig['timer']['offtime'] = server.off_time_value
             print("config change", server.off_time_value)
             myconfigfile.write(myconfig)
+
+        if configChanged:
+            for i in range(9999999):
+                pass
+            machine.reset()
     machine.reset()
 
 
